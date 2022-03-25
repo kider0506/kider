b65857998e740b15f931999bb397e72fa9c36efd
Kider <kidernj16@gmail.com>
2022-03-25 06:11:30 +0530

    First Commit

---
+++ src/main/java/com/mkide1/kider/KiderApplication.java
@@ -0,0 +1,13 @@
+package com.mkide1.kider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkide1/kider/KiderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkide1.kider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
