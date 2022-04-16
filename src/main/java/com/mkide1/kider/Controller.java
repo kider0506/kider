983631b9ba209d22ee50cb17ac112a42af0be32b
Kider <kidernj16@gmail.com>
2022-04-16 15:05:18 +0530

    Message Updated

--- src/main/java/com/mkide1/kider/Controller.java
+++ src/main/java/com/mkide1/kider/Controller.java
@@ -1,13 +1,21 @@
 package com.mkide1.kider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class Controller {
 	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 @GetMapping("/get")
 public String meth1() {
-	return "Hello";
+	logger.info("Welcome");
+	System.out.println("Hi");
+	return "Hello2";
 }
 }
